99445b800f17b85bb39f98fb32e30af4938c58ec
Christopher Fox <cfox1290@gmail.com>
2018-03-01 16:20:38 -0700

    finished base

--- src/Game/states/load.js
+++ src/Game/states/load.js
@@ -1,5 +1,13 @@
-export default function loadState(){
-    return{
-       
+export default function loadState() {
+    return {
+        preload: function () {
+            this.game.load.image('player', 'img/ship.png');
+            this.game.load.image('star', 'img/star2.png');
+            this.game.load.image('baddie', 'img/space-baddie.png');
+            this.game.load.atlas('lazer', 'img/laser.png', 'img/laser.json');
+        },
+        create: function () {
+            this.game.state.start('play');
+        }
     }
 }
